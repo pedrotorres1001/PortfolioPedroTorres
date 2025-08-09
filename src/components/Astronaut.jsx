4372f2788365b450c28bc14ae5f690d11c58432d
Pedro Torres <118758354+pedrotorres1001@users.noreply.github.com>
2025-08-09 15:47:56 +0100

    first commit

--- src/components/Astronaut.jsx
+++ src/components/Astronaut.jsx
@@ -6,7 +6,7 @@ import { useFrame } from "@react-three/fiber";
 export function Astronaut(props) {
   const group = useRef();
   const { nodes, materials, animations } = useGLTF(
-    "./assets/models/tenhun_falling_spaceman_fanart.glb"
+    "/models/tenhun_falling_spaceman_fanart.glb"
   );
   const { actions } = useAnimations(animations, group);
   useEffect(() => {
@@ -119,4 +119,4 @@ export function Astronaut(props) {
   );
 }
 
-useGLTF.preload("./assets/models/tenhun_falling_spaceman_fanart.glb");
+useGLTF.preload("/models/tenhun_falling_spaceman_fanart.glb");
