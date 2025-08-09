69ea301134dc911e5502653a3d65ee68e17ff35a
Pedro Torres <118758354+pedrotorres1001@users.noreply.github.com>
2025-08-09 01:57:05 +0100

    first commit

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import emailjs from "@emailjs/browser";
-import Alert from `../components/Alert`;
-import { Particles } from `../components/Particles`;
+import Alert from "../components/Alert";
+import { Particles } from "../components/Particles";
 const Contact = () => {
   const [formData, setFormData] = useState({
     name: "",
