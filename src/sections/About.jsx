a0950923efd568d39228b0d5c44b2e62461ac8b6
Pedro Torres <118758354+pedrotorres1001@users.noreply.github.com>
2025-08-09 15:38:49 +0100

    first commit

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
         {/* Grid 1 */}
         <div className="flex items-end grid-default-color grid-1">
           <img
-            src="assets/coding-pov.png"
+            src="/assets/coding-pov.png"
             className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
           />
           <div className="z-10">
@@ -60,17 +60,17 @@ const About = () => {
             />
             <Card
               style={{ rotate: "30deg", top: "70%", left: "70%" }}
-              image="assets/logos/csharp-pink.png"
+              image="/assets/logos/csharp-pink.png"
               containerRef={grid2Container}
             />
             <Card
               style={{ rotate: "-45deg", top: "70%", left: "25%" }}
-              image="assets/logos/dotnet-pink.png"
+              image="/assets/logos/dotnet-pink.png"
               containerRef={grid2Container}
             />
             <Card
               style={{ rotate: "-45deg", top: "5%", left: "10%" }}
-              image="assets/logos/blazor-pink.png"
+              image="/assets/logos/blazor-pink.png"
               containerRef={grid2Container}
             />
           </div>
